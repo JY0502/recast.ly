70a16ba25f76a58fbbad812da1b51e74077e400f
Kim Dang <kimdang229@gmail.com>
2020-05-27 10:14:57 -0700

    Pass spec & add --watch env

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,9 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {/* VideoListEntry is a component of Video List */}
+    {props.videos.map(video => <VideoListEntry key={video.etag} video={video} onClick={props.onClick}/>)}
   </div>
 );
 
